ebe6997a38e35a5900162cead62173f8a9ceb20d
ckul <88058944+ckuly@users.noreply.github.com>
2024-11-27 12:08:08 +0200

    Added New Classes +CarModel +Car, made migrations

--- car_service/models.py
+++ car_service/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
 
+class CarModel(models.Model):
+    """Model representing a car's model."""
+    brand = models.CharField('Brand', max_length=100)
+    model = models.CharField('Model', max_length=100)
+
+    def __str__(self):
+        return f"{self.brand} {self.model}"
+
+
 class Car(models.Model):
-    license_plate = models.CharField('License Plate', max_length=6, help_text='Enter the license plate of a car (example: XXX000)')
-    car_model_id = models.ForeignKey('Car_model', on_delete=models.SET_NULL, null=True)
+    """Model representing a car."""
+    license_plate = models.CharField('License Plate', max_length=6,
+                                     help_text='Enter the license plate of a car (example: XXX000)')
+    car_model_id = models.ForeignKey('CarModel', on_delete=models.SET_NULL, null=True)
     vin_code = models.CharField('VIN Code', max_length=17, help_text='Enter the VIN code (example: 4Y1SL65848Z411439)')
     client = models.CharField('Client', max_length=100)
 
