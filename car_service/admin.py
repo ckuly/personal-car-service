d5b39fc6fb67ab94dd3c2f19bdb99323640ad041
ckul <88058944+ckuly@users.noreply.github.com>
2024-11-27 12:46:22 +0200

    Added Order registration to admin.py

--- car_service/admin.py
+++ car_service/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Car, CarModel
+from .models import Car, CarModel, Order
 
 admin.site.register(Car)
 admin.site.register(CarModel)
+admin.site.register(Order)
