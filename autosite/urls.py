76351f14cb36ad8b40e5afaa806bd787a2fec4b1
ckul <88058944+ckuly@users.noreply.github.com>
2024-11-29 13:12:11 +0200

    New files html templates, css styling, views.py

--- autosite/urls.py
+++ autosite/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('car_service/', include('car_service.urls')),
     path('index2/', include('index2.urls')),
     path('admin/', admin.site.urls),
-]
+path('', RedirectView.as_view(url='car_service/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- car_service/views.py
+++ car_service/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import CarModel, Car, Order, Service, OrderService
+
+
 def index(request):
-    return HttpResponse("Labas, pasauli!")
+    return HttpResponse("Labas, vanagas!")
+
+
+def index(request):
+    num_cars = Car.objects.all().count()
+    num_services = Service.objects.all().count()
+    num_orders_completed = Order.objects.filter(status__exact='d').count()
+
+    context = {
+        'num_cars': num_cars,
+        'num_orders_completed': num_orders_completed,
+        'num_services': num_services,
+    }
+
+    return render(request, 'index.html', context=context)
