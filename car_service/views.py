bc4350d49f4053f8a9f80262fcf22a1c3bc89da0
ckul <88058944+ckuly@users.noreply.github.com>
2024-11-29 16:03:59 +0200

    Changes to urls.py views.py

--- car_service/views.py
+++ car_service/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-from .models import CarModel, Car, Order, Service, OrderService
+from .models import Car, Order, Service
 
 
 def index(request):
