f70c688e899c1fcbc75d8be22d47a56b4fc567b4
ckul <88058944+ckuly@users.noreply.github.com>
2024-11-29 13:18:31 +0200

    changed urls.py

--- autosite/urls.py
+++ autosite/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('car_service/', include('car_service.urls')),
     path('index2/', include('index2.urls')),
     path('admin/', admin.site.urls),
-path('', RedirectView.as_view(url='car_service/', permanent=True)),
+    path('', RedirectView.as_view(url='car_service/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
