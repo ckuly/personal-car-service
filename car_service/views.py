763c143b124d82d613608c97c8f6f87a5e0aa926
ckul <88058944+ckuly@users.noreply.github.com>
2024-11-29 14:45:29 +0200

    Changes in views.py

--- car_service/views.py
+++ car_service/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from django.shortcuts import render
-from django.http import HttpResponse
 from .models import CarModel, Car, Order, Service, OrderService
 
 
