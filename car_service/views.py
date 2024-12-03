d6e41e65871f6b8486ccfdd6c0313263bb491f90
ckul <88058944+ckuly@users.noreply.github.com>
2024-12-03 15:14:38 +0200

    order paginate'ing, car paginate'ing

--- car_service/views.py
+++ car_service/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Car, Order, Service
 from django.views import generic
+from django.core.paginator import Paginator
 
 def index(request):
     num_cars = Car.objects.all().count()
@@ -16,8 +17,9 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 def cars(request):
-
-    cars = Car.objects.all()
+    paginator = Paginator(Car.objects.all(), 3)
+    page_number = request.GET.get('page')
+    cars = paginator.get_page(page_number)
     context = {
         'cars': cars
     }
@@ -32,7 +34,7 @@ class OrderListView(generic.ListView):
     model = Order
     template_name = 'order_list.html'
     context_object_name = 'order_list'
-    paginate_by = 10
+    paginate_by = 5
 
     def get_queryset(self):
         return Order.objects.all().order_by('-date')
@@ -40,3 +42,4 @@ class OrderListView(generic.ListView):
 class OrderDetailView(generic.DetailView):
     model = Order
     template_name = 'order_detail.html'
+
