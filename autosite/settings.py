154f92f3f92b73b2c7d19fa1274b6b36769befcf
ckul <88058944+ckuly@users.noreply.github.com>
2024-12-04 14:19:40 +0200

    Registration system, still need to convert to english and change couple things

--- autosite/settings.py
+++ autosite/settings.py
@@ -125,7 +125,18 @@ STATICFILES_DIRS = [BASE_DIR / "static"]  # new
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # media folder settings
-MEDIA_ROOT = os.path.join(BASE_DIR, 'library/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'car_service/media')
 
 MEDIA_URL = '/media/'
 # print(MEDIA_ROOT) - nevenkite padebuginti, bus lengviau nepasiklysti django filesystem džiunglėse
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+# el. pašto adresas iš kurio siųsite
+EMAIL_HOST_PASSWORD = 'VerySecret'
+# slaptažodis
--- autosite/urls.py
+++ autosite/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('car_service/', include('car_service.urls')),
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='car_service/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
